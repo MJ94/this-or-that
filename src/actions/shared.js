1d817e7124f419df76bf86b8e8659585df59349b
Michael Jackson <mj94@mj94.net>
2019-06-27 18:58:59 -0500

    Setup shared action creator

---
+++ src/actions/shared.js
@@ -0,0 +1,13 @@
+import { getInitialData } from '../utils/api';
+import { receiveUsers } from './users';
+import { getQuestions } from './questions';
+import { setAuthedUser } from './authedUsers';
+
+const AUTHED_ID = 'tylermcginnis';
+
+export const handleInitialData = () => dispatch => getInitialData()
+  .then(({ users, questions }) => {
+    dispatch(receiveUsers(users));
+    dispatch(getQuestions(questions));
+    dispatch(setAuthedUser(AUTHED_ID));
+  });
