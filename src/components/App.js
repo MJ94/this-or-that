1d09790f9a8995cd329170ead0c62713565d4710
Michael Jackson <mj94@mj94.net>
2019-06-29 11:02:48 -0500

    improvement: put Login component in container

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,15 @@
 import React, { Component } from 'react';
+import { Container, Row, Col } from 'reactstrap';
 import Login from './Login';
 
 export default class App extends Component {
   render() {
     return (
-      <Login />
+      <Container>
+        <Row>
+          <Col><Login /></Col>
+        </Row>
+      </Container>
     );
   }
 }
