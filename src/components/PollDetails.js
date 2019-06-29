f34607ec134f4333f5fdb2cec2f7f026b7487820
Michael Jackson <mj94@mj94.net>
2019-06-29 16:39:06 -0500

    feat: add skeletons for rest of components

---
+++ src/components/PollDetails.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class PollDetails extends Component {
+    render() {
+        return (
+            <div>
+                Poll Details Page
+            </div>
+        )
+    }
+}
+
+export default PollDetails
