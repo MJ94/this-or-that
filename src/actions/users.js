311cb7162489c4acf996f3539bfe38b380976c66
Michael Jackson <mj94@mj94.net>
2019-06-29 13:21:23 -0500

    feat: create addUserAnswer action

--- src/actions/users.js
+++ src/actions/users.js
@@ -1,6 +1,14 @@
 export const RECEIVE_USERS = 'RECEIVE_USERS';
+export const ADD_USER_ANSWER = 'ADD_USER_ANSWER';
 
 export const receiveUsers = users => ({
   type: RECEIVE_USERS,
   users,
+});
+
+export const addUserAnswer = (authedUser, qid, answer) => ({
+  type: ADD_USER_ANSWER,
+  authedUser,
+  qid,
+  answer,
 });
