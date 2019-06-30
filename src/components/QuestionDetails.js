58bcb8b9117d640795ba37e6608d87343d42282e
Michael Jackson <mj94@mj94.net>
2019-06-30 17:46:08 -0500

    feat: saves answers

--- src/components/QuestionDetails.js
+++ src/components/QuestionDetails.js
@@ -2,31 +2,90 @@ import React, { Component } from 'react';
 import { Redirect } from 'react-router-dom';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
+import { Card, CardHeader, CardBody, CardTitle, FormGroup, Label, Input, Form, Button } from 'reactstrap';
+import { handleSaveAnswer } from '../actions/shared';
+import User from './User';
 
 class QuestionDetails extends Component {
+  state = {
+    selectedAnswer: '',
+  }
+
+  radioSelected = (e) => {
+    this.setState({
+      selectedAnswer: e.target.value,
+    });
+  }
+
+  handleSubmit = (e) => {
+    e.preventDefault();
+    this.props.saveAnswer(this.state.selectedAnswer);
+  }
+
   render() {
-    const { question } = this.props;
+    const { question, questionAuthor, authedUser } = this.props;
 
     if (!question) {
-      return <Redirect to="/NotFound" />;
+      return <Redirect to="/404" />;
     }
 
     return (
-      <div>Question Details</div>
+      <Card>
+        <CardHeader>
+          <User id={questionAuthor.id} />
+        </CardHeader>
+        <CardBody>
+          <CardTitle>This Or That?</CardTitle>
+          <Form onSubmit={this.handleSubmit}>
+            <FormGroup tag="fieldset">
+              <FormGroup check>
+                <Label check>
+                  <Input type="radio" name="radio1" value="optionOne" checked onChange={this.radioSelected} />
+                  {' '}
+                  {question.optionOne.text}
+                </Label>
+              </FormGroup>
+              <FormGroup check>
+                <Label check>
+                  <Input type="radio" name="radio1" value="optionTwo" onChange={this.radioSelected} />
+                  {' '}
+                  {question.optionTwo.text}
+                </Label>
+              </FormGroup>
+            </FormGroup>
+            <Button>Submit</Button>
+          </Form>
+        </CardBody>
+      </Card>
     );
   }
 }
 
-const mapStateToProps = ({ questions }, props) => {
+QuestionDetails.propTypes = {
+  question: PropTypes.object,
+  questionAuthor: PropTypes.object,
+  authedUser: PropTypes.string.isRequired,
+  saveAnswer: PropTypes.func.isRequired,
+};
+
+const mapStateToProps = ({ questions, users, authedUser }, props) => {
   const { id } = props.match.params;
   const question = questions[id];
+  const questionAuthor = users[question.author];
   return {
     question,
+    questionAuthor,
+    authedUser,
   };
 };
 
-QuestionDetails.propTypes = {
-  question: PropTypes.object,
+const mapDispatchToProps = (dispatch, props) => {
+  const { id } = props.match.params;
+  return {
+    saveAnswer: (answer) => {
+      dispatch(handleSaveAnswer(id, answer));
+    },
+  };
 };
 
-export default connect(mapStateToProps)(QuestionDetails);
+export default connect(mapStateToProps, mapDispatchToProps)(QuestionDetails);
