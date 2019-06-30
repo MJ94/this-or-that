b578a94ed15b836c52f0a51805842200a3130d12
Michael Jackson <mj94@mj94.net>
2019-06-29 20:33:08 -0500

    feat: setup routing

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,9 @@
 import React, { Component, Fragment } from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 import LoadingBar from 'react-redux-loading-bar';
+import { Container } from 'reactstrap';
 import { handleInitialData } from '../actions/shared';
 import Login from './Login';
 import NavBar from './NavBar';
@@ -10,23 +12,50 @@ import PollDetails from './PollDetails';
 import Leaderboard from './Leaderboard';
 import NewPoll from './NewPoll';
 
+
 class App extends Component {
   componentDidMount() {
     this.props.dispatch(handleInitialData());
   }
 
   render() {
+    const { notLoggedIn } = this.props;
+
     return (
-      <Fragment>
-        <NavBar />
-        <Login />
-      </Fragment>
+      <Router>
+        <Fragment>
+          <LoadingBar />
+          <NavBar />
+          <Container>
+            {
+              notLoggedIn ? <Route path="/" component={Login} />
+                : (
+                  <div>
+                    <Route path="/" exact component={MainPage} />
+                    <Route path="/questions/:id" component={PollDetails} />
+                    <Route path="/add" component={NewPoll} />
+                    <Route path="/leaderboard" component={Leaderboard} />
+                  </div>
+                )
+            }
+          </Container>
+        </Fragment>
+      </Router>
     );
   }
 }
 
+
+function mapStateToProps({ authedUser }) {
+  return {
+    notLoggedIn: authedUser === null,
+  };
+}
+
 App.propTypes = {
   dispatch: PropTypes.func.isRequired,
+  notLoggedIn: PropTypes.bool.isRequired,
 };
 
-export default connect()(App);
+
+export default connect(mapStateToProps)(App);
