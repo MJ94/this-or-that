2f15aed170d33b43035a722c14feef9f0f3f97dd
Michael Jackson <mj94@mj94.net>
2019-06-29 19:18:45 -0500

    chore: import other components for use

--- src/components/App.js
+++ src/components/App.js
@@ -4,6 +4,10 @@ import PropTypes from 'prop-types';
 import { handleInitialData } from '../actions/shared';
 import Login from './Login';
 import NavBar from './NavBar';
+import MainPage from './MainPage';
+import PollDetails from './PollDetails';
+import Leaderboard from './Leaderboard';
+import NewPoll from './NewPoll';
 
 class App extends Component {
   componentDidMount() {
