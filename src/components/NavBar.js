1972c0de18ad778e0835499957d36df74b9ab09a
Michael Jackson <mj94@mj94.net>
2019-06-29 23:45:57 -0500

    feat: add logout button

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,6 +1,6 @@
 /* eslint-disable react/no-access-state-in-setstate */
 /* eslint-disable react/destructuring-assignment */
-import React from 'react';
+import React, { Component, Fragment } from 'react';
 import {
   Collapse,
   Navbar,
@@ -10,46 +10,75 @@ import {
   NavItem,
   NavLink,
 } from 'reactstrap';
+import { Link } from 'react-router-dom';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+import { unsetAuthedUser } from '../actions/authedUser';
 
-class NavBar extends React.Component {
-  constructor(props) {
-    super(props);
-
-    this.toggle = this.toggle.bind(this);
-    this.state = {
-      isOpen: false,
-    };
+class NavBar extends Component {
+  state = {
+    isOpen: false,
   }
 
-  toggle() {
+  toggle = () => {
     this.setState({
       isOpen: !this.state.isOpen,
     });
   }
 
   render() {
+    const { isloggedIn } = this.props;
+
     return (
       <div>
         <Navbar color="light" light expand="md">
-          <NavbarBrand href="/">Would You Rather</NavbarBrand>
-          <NavbarToggler onClick={this.toggle} />
-          <Collapse isOpen={this.state.isOpen} navbar>
-            <Nav className="ml-auto" navbar>
-              <NavItem>
-                <NavLink href="/">Home</NavLink>
-              </NavItem>
-              <NavItem>
-                <NavLink href="/leaderboard">Leaderboard</NavLink>
-              </NavItem>
-              <NavItem>
-                <NavLink href="/add">Add Questions</NavLink>
-              </NavItem>
-            </Nav>
-          </Collapse>
+          <NavbarBrand tag={Link} to="/">This Or That</NavbarBrand>
+          {isloggedIn
+            && (
+              <Fragment>
+                <NavbarToggler onClick={this.toggle} />
+                <Collapse isOpen={this.state.isOpen} navbar>
+                  <Nav className="ml-auto" navbar>
+                    <NavItem>
+                      <NavLink tag={Link} to="/">Home</NavLink>
+                    </NavItem>
+                    <NavItem>
+                      <NavLink tag={Link} to="/leaderboard">Leaderboard</NavLink>
+                    </NavItem>
+                    <NavItem>
+                      <NavLink tag={Link} to="/add">Add Questions</NavLink>
+                    </NavItem>
+                    <NavItem>
+                      <NavLink onClick={this.props.logout}>Logout</NavLink>
+                    </NavItem>
+                  </Nav>
+                </Collapse>
+              </Fragment>
+            )
+          }
         </Navbar>
       </div>
     );
   }
 }
 
-export default NavBar;
+NavBar.propTypes = {
+  logout: PropTypes.func.isRequired,
+  isloggedIn: PropTypes.bool.isRequired,
+};
+
+function mapStateToProps({ authedUser }) {
+  return {
+    isloggedIn: authedUser !== null,
+  };
+}
+
+function mapDispatchToProps(dispatch) {
+  return {
+    logout: () => {
+      dispatch(unsetAuthedUser());
+    },
+  };
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(NavBar);
