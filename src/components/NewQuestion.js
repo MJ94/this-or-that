d2fc939671ae57fa094408a1678247062e469812
Michael Jackson <mj94@mj94.net>
2019-06-30 20:23:57 -0500

    Begin NewQuestion component

--- src/components/NewQuestion.js
+++ src/components/NewQuestion.js
@@ -1,11 +1,60 @@
-import React, { Component, Fragment } from 'react';
+import React, { Component } from 'react';
+import { Card, CardBody, CardTitle, FormGroup, Label, Input, Form, Button } from 'reactstrap';
 
 class NewQuestion extends Component {
+  state = {
+    optionOne: '',
+    optionTwo: '',
+  }
+
+  handleOptionOneChange = (e) => {
+    e.preventDefault();
+    this.setState({
+      optionOne: e.target.value,
+    });
+  }
+
+  handleOptionTwoChange = (e) => {
+    e.preventDefault();
+    this.setState({
+      optionTwo: e.target.value,
+    });
+  }
+
+  handleSubmit = (e) => {
+    e.preventDefault();
+  }
+
   render() {
+    const { optionOne, optionTwo } = this.state;
     return (
-      <Fragment>
-        <div>New Question Page</div>
-      </Fragment>
+      <Card>
+        <CardBody>
+          <CardTitle>Would you rather...?</CardTitle>
+          <Form onSubmit={this.handleSubmit}>
+            <FormGroup>
+              <Label for="optionOne">Option One</Label>
+              <Input
+                type="text"
+                value={optionOne}
+                onChange={this.handleOptionOneChange}
+                placeholder="Type option one here"
+              />
+            </FormGroup>
+            <FormGroup>
+              <Label for="optionTwo">Option Two</Label>
+              <Input
+                type="text"
+                name="optionTwo"
+                value={optionTwo}
+                onChange={this.handleOptionTwoChange}
+                placeholder="Type option two here"
+              />
+            </FormGroup>
+            <Button disabled={optionOne === '' || optionTwo === ''}>Submit</Button>
+          </Form>
+        </CardBody>
+      </Card>
     );
   }
 }
